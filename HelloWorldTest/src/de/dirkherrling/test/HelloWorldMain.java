da288c005b4bd36ff22a61ae9ce678c8e93d7cc6
pe12 <pe12@139.174.77.54>
2019-05-22 09:58:42 +0200

    initial

---
+++ HelloWorldTest/src/de/dirkherrling/test/HelloWorldMain.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package de.dirkherrling.test;
+
+/**
+ * @author pe12
+ *
+ */
+public class HelloWorldMain {
+	
+	public void hello1() {
+		System.out.println("Hello World 1");
+		System.out.println("Hello World 2");
+	}
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new HelloWorldMain().hello1();
+	}
+
+}
